bbadd70019f12cfa7d971de05d864cd230627ef3
sobolmatias <sobolm@hotmail.com>
2023-03-05 20:45:46 -0300

    version-3

--- js/past.js
+++ js/past.js
@@ -2,6 +2,7 @@ console.log(data);
 const eventos = data.events
 console.log(eventos);
 
+//CARD
 let currentDate = data.currentDate;
 let cardEventos = []
 
@@ -30,3 +31,30 @@ function printCards(){
 }
 
 printCards();
+
+//CATEGORIAS
+let categories = []
+console.log(eventos.forEach(each => { 
+    if (!categories.includes(each.category))
+    categories.push(each.category)
+}));
+console.log(categories);
+
+let templeteCategory = []
+
+function printCategory(){
+    for (let each of categories){
+    let listcategory =
+        `
+        <fieldset class="p-2 m-2">
+            <label class="contact-label" for="${each}">${each}</label>
+            <input onclick="captureData()" class="class_checks contact-input" type="checkbox" name="categ" value="${each}" id="${each}"> 
+        </Fieldset>
+        `
+        templeteCategory.push(listcategory);       
+    }
+    let basecateg = document.getElementById('categpast');
+    basecateg.innerHTML = templeteCategory.join('')
+}
+
+printCategory();
